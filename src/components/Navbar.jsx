0962802a01a3dff863369c4239dc681b220fac26
Jodi Jonatan <jdi102350@gmail.com>
2025-10-21 14:28:45 +0700

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Nama Aplikasi</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-function App() {
+export default function Home() {
   return (
     <div className="bg-[url(/background.png)] min-h-screen bg-cover flex justify-center pt-32">
       <div>
-        <h1 className="text-white text-5xl font-bold">Penguin Ngebut</h1>
+        <h1 className="text-white text-5xl font-bold">NAMA APLIKASI</h1>
         <p className="text-center text-slate-200 text-2xl">
           “Smart Drive Smart Design”
         </p>
@@ -10,5 +10,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
